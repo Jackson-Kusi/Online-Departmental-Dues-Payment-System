0cbe0ffd514a4eaa0d90848cd2d05bac25fb25c2
Kusi Jackson <kusijackson5@gmail.com>
2023-04-29 18:37:22 -0700

    welcome page

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CITSA-DUES-PAYMENT</title>
+
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: rgb(48, 48, 107);
+        }
+
+        .main {
+            margin-top: 60px;
+            padding: 50px;
+            background-color: white;
+            width: 400px;
+            height: 100%;
+        }
+
+        p {
+            font-size: 20px;
+            font-weight: bold;
+            line-height: 1.5;
+        }
+
+        span {
+            font-size: 24px;
+            margin-top: 10px;
+        }
+
+        button {
+            padding: 10px;
+            margin: 5px;
+            border: none;
+            background-color: #031435;
+            color: white;
+            font-size: 18px;
+        }
+        img {
+            width: 200px;
+            margin-bottom: -20px;
+            margin-top: -20px;
+        }
+        h1 {
+            color: red;
+        }
+
+    </style>
+</head>
+<body>
+    <center>
+        <div class="main">
+            <p>WELCOME TO</p>
+            <img src="images/CITSA Logo.png" alt="">
+            <p><span> CITSA DUES PAYMENT SYSTEM</span></p>
+            <h1>LOGIN AS</h1>
+            <a href="admin_login.php"><button>ADMIN</button></a>
+            <a href="user_login.php"><button>STUDENT</button></a>        </div>
+    </center>
+   
+</body>
+</html>
